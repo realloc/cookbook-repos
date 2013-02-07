3d5eeed3eda0cfac29bc29fad0036f3f7504d21b
Stanislav Bogatyrev <realloc@realloc.spb.ru>
2013-02-07 22:48:54 +0400

    Use release repo for jenkins-debian-glue

--- recipes/jenkins-debian-glue.rb
+++ recipes/jenkins-debian-glue.rb
@@ -7,9 +7,9 @@ case node['platform']
 when "debian","ubuntu"
   apt_repository "jenkins-debian-glue" do
     uri "http://jenkins.grml.org/debian"
-    distribution "jenkins-debian-glue"
+    distribution "jenkins-debian-glue-release"
     components ["main"]
     key "http://jenkins.grml.org/debian/C525F56752D4A654.asc"
-    deb_src false
+    deb_src true
   end
 end
