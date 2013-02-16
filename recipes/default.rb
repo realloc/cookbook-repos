2beb9431324257bf841a00af0502ff3dc8c93de6
Stanislav Bogatyrev <realloc@realloc.spb.ru>
2013-02-16 15:39:34 +0400

    Add repo config via attributes.

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,5 +4,17 @@
 #
 
 node['repos'].each do |repo|
-  include_recipe "repos::#{repo}"
+  if repo.is_a?(String) then
+    include_recipe "repos::#{repo}"
+  else
+    case repo["type"]
+    when "apt" then
+      apt_repository repo["name"] do
+        %w{uri distribution components arch deb_src
+           keyserver key cookbook cache_rebuild}.each do |attr|
+          send(attr, repo[attr])  if repo[attr]
+        end
+      end
+    end
+  end
 end
